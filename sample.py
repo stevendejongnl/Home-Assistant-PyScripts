e31c18c7a8b875df8a1c0d23e69c89deee0100d5
Steven de jong 🍟 <sdejong@cloudsuite.com>
2024-06-29 18:21:41 +0200

    Add pyscript sample pytest

--- sample.py
+++ sample.py
@@ -8,8 +8,13 @@ handler.setFormatter(formatter)
 logger.addHandler(handler)
 
 
-def sample_script():
-    logger.error("Log sample error")
+def sample_script(some_input: str) -> bool:
+    if some_input == 'yes':
+        logger.info("Sample script False because yes")
+        return False
+
+    logger.info("Sample script True because not yes")
+    return True
 
 
-sample_script()
+sample_script('not yes')
---
+++ test-sample.py
@@ -0,0 +1,10 @@
+import pytest
+from pyscript.sample import sample_script
+
+
+def test_sample_script_yes_should_return_false():
+    assert not sample_script('yes')
+
+
+def test_sample_script_not_yes_should_return_true():
+    assert sample_script('not yes')
