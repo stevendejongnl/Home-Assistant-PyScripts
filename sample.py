b4514ce9f03a8f7edfdd5705461b56dd079dbb53
Steven de jong 🍟 <sdejong@cloudsuite.com>
2024-06-29 18:03:31 +0200

    Add sample script

---
+++ sample.py
@@ -0,0 +1,15 @@
+import logging
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+handler = logging.StreamHandler()
+formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+
+
+def sample_script():
+    logger.error("Log sample error")
+
+
+sample_script()
